52dfb45bbd85dc39f27efa6e2858e980d6f6c44e
Henri de Jong <henri.dejong@mobiliteitsfabriek.nl>
2015-08-29 10:04:17 +0200

    Added invalidCommandException

---
+++ src/Domain/Game/InvalidCommandException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri
+ * Date: 8/29/15
+ * Time: 9:58 AM
+ */
+
+namespace Wecamp\FlyingLiqourice\Domain\Game;
+
+
+class InvalidCommandException {
+
+}
